04696c9c32e0b47fe90c8befbe9dd7076d874cdd
Olaf Surgut <olaf.ideas@gmail.com>
2022-02-25 13:33:24 +0100

    buzzer + sd card enjoyer

--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,108 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+
 #include "api.hpp"
 
 #define THERM_PIN 32
+#define SD_CS_PIN 27
+#define BUZZ_PIN  15
 
-/*
-#define TMP_PIN_1 23
-#define HUM_PIN_1 13
-
-OneWire oneWire(TMP_PIN_1);
-
-DallasTemperature tmp_sensor(&oneWire);
-Adafruit_BMP280   bar_sensor;
-DHT               hum_sensor(HUM_PIN_1, DHT11);
-Adafruit_MPU6050  gyr_sensor;
-
-String neoexample = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47";
-
-void calibrate_gyro();
-
-void parseGPS(const String& s) {
-
-  float time;
-  float latitute;
-  float longitude;
-  int fix_quality;
-  int number_of_satellites;
-  float horizontal_precision;
-  float altitude;
-
-  sscanf(s.c_str(), "$GPGGA,%f,%f,N,%f,E,%d,%d,%f,%f", &time, &latitute, &longitude, &fix_quality, &number_of_satellites, &horizontal_precision, &altitude);
-
-  Serial.print("GMP :");
-  Serial.printf(" time = %f, lat = %f, lon = %f, fix = %d, #sat = %d, pre = %f, alt = %f\n", time, latitute, longitude, fix_quality, number_of_satellites, horizontal_precision, altitude);
-}
-
-void setup() {
-
-
-  Serial.begin(9600);
-  Serial2.begin(9600);
-
-  tmp_sensor.begin();
-
-  if (!bar_sensor.begin()) {
-    Serial.println("Count not find BMP280 sensor");
-  }
-  bar_sensor.setSampling(Adafruit_BMP280::MODE_NORMAL,    
-                         Adafruit_BMP280::SAMPLING_X2,    
-                         Adafruit_BMP280::SAMPLING_X16,   
-                         Adafruit_BMP280::FILTER_X16,     
-                         Adafruit_BMP280::STANDBY_MS_500);
-
-  hum_sensor.begin();
-
-  if (!gyr_sensor.begin()) {
-    for (;;) {
-      Serial.println("panic: gyr_sensor");
-      Serial.flush();
-    }
-  }
-
-  gyr_sensor.setAccelerometerRange(MPU6050_RANGE_8_G);
-  gyr_sensor.setGyroRange(MPU6050_RANGE_1000_DEG);
-  gyr_sensor.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  calibrate_gyro();
-  // gyr_sensor.setgyr_sensorConfig(2);
-  // gyr_sensor.setAccConfig(2);
-  //gyr_sensor.calcOffsets();
-
-  Serial.println("cansus inited");
-}
-*/
-
+File cansat_data;
 API api;
 
 void setup() {
   Serial.begin(9600);
-  api.init();
-}
 
+  // if (!SD.begin(SD_CS_PIN)) {
+  //   Serial.println("SD card inicialization failed");
+  //   for (;;) {
+  //     Serial.println("Cannot init SD card\n");
+  //   }
+  // }
+
+  // 2,1 kHz
+
+
+  ledcAttachPin(BUZZ_PIN, 0);
+  
+  // String filename = "cansat";
+  // int file_number = 0;
+
+  // // dont overwrite existing files
+  // while (SD.exists((char*) (void*) (filename + String(file_number) + ".dat").c_str())) {
+  //   file_number++;
+  // }
+
+  // filename = filename + String(file_number) + ".dat";
+
+  // Serial.println("Opening: " + filename);
+
+  // cansat_data = SD.open("/cansat0.dat", FILE_WRITE);
+  // // cansat_data = SD.open((char*) (void*) filename.c_str(), FILE_WRITE);
+
+  // if (!cansat_data) {
+  //   Serial.println("Openinng file in SD card failed");
+  //   cansat_data.close();
+  //   for (;;) {
+  //     Serial.println("Cannot open a file: " + filename);
+  //   }
+  // }
+
+  // api.init();
+}
 void loop() {
 
-  api.gps_update();
+  ledcWriteNote(0, NOTE_C, 4);
+  delay(500);
+  ledcWriteNote(0, NOTE_D, 4);
+  delay(500);
+  ledcWriteNote(0, NOTE_E, 4);
+  delay(500);
+  ledcWriteNote(0, NOTE_F, 4);
+  delay(500);
+  ledcWriteNote(0, NOTE_G, 4);
+  delay(500);
+  ledcWriteNote(0, NOTE_A, 4);
+  delay(500);
+  ledcWriteNote(0, NOTE_B, 4);
+  delay(500);
+  ledcWriteNote(0, NOTE_C, 5);
+  delay(500);
 
-  Serial.printf("TMP DS18: %f\n", api.tmp_ds18());
-  Serial.printf("TMP DHT : %f\n", api.tmp_dht());
-  Serial.printf("TMP BMP : %f\n", api.tmp_bmp());
-  // Serial.printf("TMP GYR : %f\n", api.tmp_gyr());
-  Serial.printf("HUM DHT : %f\n", api.hum_dht());
-  Serial.printf("BAR BMP : %f\n", api.bar_bmp());
-  Serial.printf("GPS TIME: %f\n", api.gps_time());
-  Serial.printf("GPS_LAT : %f\n", api.gps_latitute());
-  Serial.printf("GPS_LON : %f\n", api.gps_longitude());
-  Serial.printf("GPS_QUA : %d\n", api.gps_quality());
-  Serial.printf("GPS_NUM : %d\n", api.gps_number_of_satellites());
-  Serial.printf("GPS_PREC: %f\n", api.gps_horizontal_precision());
-  Serial.printf("GPS_ALT : %f\n", api.gps_altitude());
-  Serial.printf("----------\n");
 
-  api.lora_send();
+  return;
 
-  delay(300);
+  const int len = 4;
+  
+  const unsigned char msg[len] = {'s', 'u', 's', '\n'};
+
+  cansat_data.write(msg, len);  
+
+  Serial.print("writing to ");
+  Serial.println(cansat_data.name());
+
+  cansat_data.flush();
+  // api.gps_update();
+
+  // Serial.printf("TMP DS18: %f\n", api.tmp_ds18());
+  // Serial.printf("TMP DHT : %f\n", api.tmp_dht());
+  // Serial.printf("TMP BMP : %f\n", api.tmp_bmp());
+  // // Serial.printf("TMP GYR : %f\n", api.tmp_gyr());
+  // Serial.printf("HUM DHT : %f\n", api.hum_dht());
+  // Serial.printf("BAR BMP : %f\n", api.bar_bmp());
+  // Serial.printf("GPS TIME: %f\n", api.gps_time());
+  // Serial.printf("GPS_LAT : %f\n", api.gps_latitute());
+  // Serial.printf("GPS_LON : %f\n", api.gps_longitude());
+  // Serial.printf("GPS_QUA : %d\n", api.gps_quality());
+  // Serial.printf("GPS_NUM : %d\n", api.gps_number_of_satellites());
+  // Serial.printf("GPS_PREC: %f\n", api.gps_horizontal_precision());
+  // Serial.printf("GPS_ALT : %f\n", api.gps_altitude());
+  // Serial.printf("----------\n");
+
+  // api.lora_send();
+
+  delay(100000);
 
   return;
 
